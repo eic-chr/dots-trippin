943cd7ec73512452e6de8159427963af3cf0db6a
Christian Eickhoff <christian@ewolutions.de>
2026-02-05 07:34:18 +0100

    fix substituters

--- nix/flake.nix
+++ nix/flake.nix
@@ -12,11 +12,11 @@
     substituters = [
       "https://ncps.lan.eickhoff-it.net"
       # Query the mirror of USTC first, and then the official cache.
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
     ];
     extra-trusted-public-keys = [
-      "ncps.lan.eickhoff-it.net:kPV9cRk5SA/oqFPzY4lTEWv1/2fs1Q4AaNX2x872rFM="
+      "ncps.lan.eickhoff-it.net-1:kPV9cRk5SA/oqFPzY4lTEWv1/2fs1Q4AaNX2x872rFM="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 
