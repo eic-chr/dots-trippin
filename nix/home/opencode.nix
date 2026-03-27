c3c394ff26cb67ddbdd79c50c907e1b788ffe9de
Christian Eickhoff <christian.eickhoff@huk-coburg.de>
2026-03-27 15:35:12 +0100

    compliance

--- nix/home/opencode.nix
+++ nix/home/opencode.nix
@@ -8,28 +8,6 @@
         openTelemetry = false;
         mcp_timeout = 600000;
       };
-      provider = {
-        openai-compatible = {
-          npm = "@ai-sdk/openai-compatible";
-          name = "HUK Provider";
-          options = {
-            baseURL =
-              "https://openwebui.kollabproxy.de.dmz.tuhuk.de:8443/api/v1";
-          };
-          models = {
-            "aws.eu.anthropic.claude-sonnet-4-20250514-v1:0" = {
-              name = "aws.claude-sonnet-4-20250514-v1:0";
-            };
-            "aws.eu.anthropic.claude-opus-4-6-v1" = {
-              name = "aws.eu.anthropic.claude-opus-4-6-v1";
-            };
-            "aws.eu.anthropic.claude-sonnet-4-6" = {
-              name = "aws.eu.anthropic.claude-sonnet-4-6";
-            };
-            gpt5 = { name = "gpt5"; };
-          };
-        };
-      };
     };
   };
 
